f0b45e629619a06d24a509e95088c6315ef67362
seiyamori <seiyamori95@gmail.com>
2024-03-29 15:08:47 +0000

    New project

---
+++ IFC-Editor/IFC-Editor/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace IFC_Editor
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
