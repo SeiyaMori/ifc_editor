df1dbed515683818a486bc18423604af1d164542
seiyamori <seiyamori95@gmail.com>
2024-03-29 15:28:19 +0000

    Demo app done

--- IFC-Editor/IFC-Editor/MainWindow.xaml.cs
+++ IFC-Editor/IFC-Editor/MainWindow.xaml.cs
@@ -20,5 +20,14 @@ namespace IFC_Editor
         {
             InitializeComponent();
         }
+
+        private void ButtonAddName_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtName.Text) && !lstNames.Items.Contains(txtName.Text))
+            {
+                lstNames.Items.Add(txtName.Text);
+                txtName.Clear();
+            }
+        }
     }
 }
