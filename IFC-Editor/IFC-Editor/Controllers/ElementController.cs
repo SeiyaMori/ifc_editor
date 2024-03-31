ba646539605b2f9fd42c2f32ef9d2e67acdc9155
seiyamori <seiyamori95@gmail.com>
2024-03-31 17:06:32 +0100

    Refactor Done

--- IFC-Editor/IFC-Editor/Controllers/ElementController.cs
+++ IFC-Editor/IFC-Editor/Controllers/ElementController.cs
@@ -1,12 +1,68 @@
-﻿using System;
+﻿using IFC_Editor.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xbim.Ifc4.ProductExtension;
+using Xbim.Ifc4.Interfaces;
+
 
 namespace IFC_Editor.Controllers
 {
     internal class ElementController
     {
+        public ElementContext Db { get; }
+
+        public ElementController()
+        {
+            Db = new ElementContext();
+        }
+
+        public void SaveDb()
+        {
+            Db.SaveChanges();
+        }
+
+        public void CreateElement(string ElementId, string Name, string ElementType)
+        {
+            Db.Add(new Element { ElementId = ElementId, Name = Name, ElementType = ElementType, Exclude = false });
+        }
+
+        public List<Element> GetAllElements()
+        {
+            List<Element> elements = Db.Elements.OrderBy(b => b.ElementId).ToList();
+            return elements;
+        }
+
+        public void DeleteElement(Element Element)
+        {
+            Db.Remove(Element);
+        }
+
+        public string GetElementType(IfcBuildingElement item)
+        {
+            if (item is IIfcWall wall)
+            {
+                return "Wall";
+            }
+            else if (item is IIfcDoor door)
+            {
+                return "Door";
+            }
+            else if (item is IIfcWindow window)
+            {
+                return "Window";
+            }
+            else if (item is IIfcRoof roof)
+            {
+                return "Roof";
+            }
+            else if (item is IIfcFace face)
+            {
+                return "Face";
+            }
+            return "Unknown";
+        }
     }
 }
--- IFC-Editor/IFC-Editor/MainWindow.xaml.cs
+++ IFC-Editor/IFC-Editor/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using IFC_Editor.Models;
+using IFC_Editor.Controllers;
 using PropertyTools.Wpf;
 using System.Reflection.Metadata;
 using System.Text;
@@ -12,7 +13,6 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Xbim.Ifc;
-using Xbim.Ifc4.Interfaces;
 using Xbim.Ifc4.ProductExtension;
 using Xbim.ModelGeometry.Scene;
 
@@ -31,34 +31,29 @@ namespace IFC_Editor
             IfcStore model = IfcStore.Open(@"C:\Users\Seiya\Desktop\example_building.ifc");
             var objs = model.Instances.OfType<IfcBuildingElement>();
 
-            ElementContext db = new ElementContext();
+            // Element Controller
+            ElementController elementController = new ElementController();
+
 
             // Clear SQLite
             
-            List<Element> ExistingItems = LoadElementData();
-            foreach (Element item in ExistingItems)
+            List<Element> existingItems = elementController.GetAllElements();
+            foreach (Element item in existingItems)
             {
-                DeleteElement(db, item);
+                elementController.DeleteElement(item);
             }
 
             // Create Elements from Ifc elements
             foreach (var item in objs)
             {
                 //System.Diagnostics.Debug.WriteLine(item);
-                CreateElement(db, item.GlobalId, item.Name, GetElementType(item));
+
+                elementController.CreateElement(item.GlobalId, item.Name, elementController.GetElementType(item));
             }
-            db.SaveChanges();
-
-            /*
-            System.Diagnostics.Debug.WriteLine("HELLO");
-            System.Diagnostics.Debug.WriteLine(model.ToString());
-            var context = new Xbim3DModelContext(model);
-            context.CreateContext();
-            DrawingControl.Model = model;
-            DrawingControl.LoadGeometry(model);*/
+            elementController.SaveDb();
 
 
-            DG.ItemsSource = LoadElementData();
+            DG.ItemsSource = elementController.GetAllElements();
 
             // 3D view
             Loaded += MainWindow_Loaded;
@@ -85,69 +80,12 @@ namespace IFC_Editor
             var context = new Xbim3DModelContext(model);
             context.CreateContext();
             ModelProvider.ObjectInstance = model;
-            //ModelProvider.Refresh();
         }
 
         private void DrawingControl_MouseMove(object sender, MouseEventArgs e)
         {
 
         }
-
-        private void CreateElement(ElementContext db, string ElementId, string Name, string ElementType)
-        {
-            db.Add(new Element { ElementId = ElementId, Name = Name, ElementType = ElementType, Exclude = false});
-        }
-
-        private List<Element> LoadElementData()
-        {
-            var db = new ElementContext();
-            List<Element> elements = db.Elements.OrderBy(b => b.ElementId).ToList();
-            return elements;
-        }
-
-        private void DeleteElement(ElementContext db, Element Element)
-        {
-            db.Remove(Element);
-        }
-
-        private string GetElementType(IfcBuildingElement item)
-        {
-            if (item is IIfcWall wall)
-            {
-                return "Wall";
-            }
-            else if (item is IIfcDoor door)
-            {
-                return "Door";
-            }
-            else if (item is IIfcWindow window)
-            {
-                return "Window";
-            }
-            else if (item is IIfcRoof roof)
-            {
-                return "Roof";
-            }
-            else if (item is IIfcFace face)
-            {
-                return "Face";
-            }
-            return "Unknown";
-            /*
-            var properties = item.IsDefinedBy
-            .Where(r => r.RelatingPropertyDefinition is IIfcPropertySet)
-            .SelectMany(r => ((IIfcPropertySet)r.RelatingPropertyDefinition).HasProperties)
-            .OfType<IIfcPropertySingleValue>();
-            foreach (var property in properties)
-            {
-                System.Diagnostics.Debug.WriteLine(property.NominalValue);
-                if (property.Name == "Type")
-                {
-                    return property.NominalValue.ToString();
-                }
-            }
-                
-            return "No value";*/
-        }
+        
     }
 }
